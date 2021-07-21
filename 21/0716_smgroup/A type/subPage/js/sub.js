b142c7c8d884bdb3cccf12dfc13eeccfce096029
choichansik <ckstlr0828@frameout.co.kr>
2021-07-21 14:49:13 +0900

    dd
    dd

--- 21/0716_smgroup/A type/subPage/js/sub.js
+++ 21/0716_smgroup/A type/subPage/js/sub.js
@@ -1,13 +1,21 @@
 $(document).ready(function(){
 
-    $(".clickhere").click(function(){
-        if($(".clickmenu").css("display") === "none"){
-            $(".clickmenu").css("display", "block");
-            $(".clickhere").removeAttr("id", "clickhereId");
-            
-        }else{
-            $(".clickmenu").css("display", "none");
-            $(".clickhere").attr("id", "clickhereId");
+    var contentE = function(){
+        $(".clickhere").click(function(){
+            if($(".clickmenu").css("display") === "none"){
+                $(".clickmenu").css("display", "block");
+                $(".clickhere").removeAttr("id", "clickhereId");
+            }else{
+                $(".clickmenu").css("display", "none");
+                $(".clickhere").attr("id", "clickhereId");
+            }
+        });
+    }
+    contentE();
+
+    $(window).on('scroll',function(){
+        if($(".clickmenu").css("display") === "block"){
+            $(".clickmenu").trigger("click");
         }
     });
 });
